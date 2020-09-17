17deb1bcde534fd9f08b0f0cd6be1d2495172a29
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-09-17 14:02:37 -0300

    Updates: Header styles of stack navigator

--- src/routes/LoginStack.tsx
+++ src/routes/LoginStack.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import CreateAccountPF from '../pages/CreateAccountPF';
 import CreateAccountPJ from '../pages/CreateAccountPJ';
+import Login from '../pages/Login';
 import WelcomeScreen from '../pages/WelcomeScreen';
 
 const { Navigator, Screen } = createStackNavigator();
@@ -10,11 +11,21 @@ const LoginStack = (): JSX.Element => {
   return (
     <Navigator
       screenOptions={{
-        headerShown: false,
+        headerTitle: '',
+        headerStyle: {
+          backgroundColor: '#000',
+          elevation: 0,
+        },
+        headerTintColor: '#ffffff',
       }}
       initialRouteName="WelcomeScreen"
     >
-      <Screen name="WelcomeScreen" component={WelcomeScreen} />
+      <Screen
+        name="WelcomeScreen"
+        options={{ headerShown: false }}
+        component={WelcomeScreen}
+      />
+      <Screen name="Login" options={{ headerShown: false }} component={Login} />
       <Screen name="CreateAccountPJ" component={CreateAccountPJ} />
       <Screen name="CreateAccountPF" component={CreateAccountPF} />
     </Navigator>
